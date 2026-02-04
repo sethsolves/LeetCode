ad2687dc57174b1a8ae854f690ec249fc334fcd5
Seth <142804088+sethsolves@users.noreply.github.com>
2026-02-04 12:30:23 +0330

    Directory updated.

--- LeetCode75/Array - String/1768.merge-strings-alternately.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    @staticmethod
-    def merge_alternately(word1: str, word2: str) -> str:
-        merged = []
-        for char1, char2 in zip(word1, word2):
-            merged.append(char1)
-            merged.append(char2)
-        remaining = word1[len(word2):] + word2[len(word1):]
-        return "".join(merged) + remaining
